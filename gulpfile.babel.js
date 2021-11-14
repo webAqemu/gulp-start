18f305c4befbffcaeff58fa8a68d861651f1dfbb
webAqemu <notych2812@gmail.com>
2021-11-14 12:59:30 +0300

    added pug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,25 @@
+import gulp from "gulp";
 import config from "./gulp/config";
+import clean from "./gulp/tasks/clean";
+import { scriptsBuild, scriptsWatch } from "./gulp/tasks/scripts";
+import { pugBuild, pugWatch } from "./gulp/tasks/pug";
+import server from "./gulp/tasks/server";
 
 config.setEnv();
 
+export const build = gulp.series(
+  clean,
+  gulp.parallel(
+    scriptsBuild,
+    pugBuild
+  )
+);
+
+export const watch = gulp.series(
+  //build,
+  server,
+  gulp.parallel(
+    scriptsWatch,
+    pugWatch
+  )
+);
